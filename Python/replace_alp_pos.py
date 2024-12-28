7523c370a8025e709c331c243712e1ca74068252
Kenan Ben <gazokenan@gmail.com>
2024-12-28 18:44:21 +0800

    alt: replace-alph-position-solution

--- Python/replace_alp_pos.py
+++ Python/replace_alp_pos.py
@@ -4,3 +4,8 @@ def alphabet_position(text):
         if char.isalpha():  
             result.append(str(ord(char.lower()) - 96)) 
     return ' '.join(result)
+
+#or 
+""" def alphabet_position(text):
+    return ' '.join(str(ord(char) - 96) for char in text.lower() if char.isalpha())
+ """
