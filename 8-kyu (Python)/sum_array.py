1606a1dab92edd5cea33f43d650355d96e8cbb1e
Kenan Ben <gazokenan@gmail.com>
2024-12-28 18:22:01 +0800

    alt: sum-array-solution

--- 8-kyu (Python)/sum_array.py
+++ 8-kyu (Python)/sum_array.py
@@ -9,3 +9,7 @@ def array_plus_array(arr1,arr2):
 #or 
 """ def array_plus_array(arr1,arr2):
     return sum(arr1) + sum(arr2) """
+
+#or
+""" def array_plus_array(arr1,arr2):
+    return sum(arr1 + arr2) """
