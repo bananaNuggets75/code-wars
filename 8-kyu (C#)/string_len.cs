c6b184ecce7495c49b358511338fba9c3c50727e
Kenan Ben <gazokenan@gmail.com>
2024-12-28 12:47:01 +0800

    alt: string-len-solution

--- 8-kyu (C#)/string_len.cs
+++ 8-kyu (C#)/string_len.cs
@@ -27,3 +27,22 @@ public static class Kata
     }
 }
 
+/* using System;
+
+public static class Kata
+{
+    public static string Likes(string[] name)
+    {
+        int count = name?.Length ?? 0; // Handle null array
+
+        return count switch
+        {
+            0 => "no one likes this",
+            1 => $"{name[0]} likes this",
+            2 => $"{name[0]} and {name[1]} like this",
+            3 => $"{name[0]}, {name[1]} and {name[2]} like this",
+            _ => $"{name[0]}, {name[1]} and {count - 2} others like this"
+        };
+    }
+} */
+
