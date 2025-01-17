8fe301f89f715f7a778c4b4eb30afcadf685a1fa
Kenan Ben <gazokenan@gmail.com>
2025-01-17 11:26:40 +0800

    pig-latin-alt-solution-2.0

--- Python/pig_latin.py
+++ Python/pig_latin.py
@@ -13,3 +13,9 @@ def pig_it(text):
     lst = text.split()
     return ' '.join( [word[1:] + word[:1] + 'ay' if word.isalpha() else word for word in lst])
 """
+
+# or 
+"""import re
+
+def pig_it(text):
+    return re.sub(r'([a-z])([a-z]*)', r'\2\1ay', text, flags=re.I)"""
