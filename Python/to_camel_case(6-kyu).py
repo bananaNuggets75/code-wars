997059b85667d19c8e640e44ffb67dbb6e9258f9
Kenan Ben <gazokenan@gmail.com>
2025-01-18 20:45:24 +0800

    to-camel-case-alt-solution

--- Python/to_camel_case(6-kyu).py
+++ Python/to_camel_case(6-kyu).py
@@ -3,3 +3,7 @@ def to_camel_case(text):
     for i in range(1, len(words)):
         words[i] = words[i].capitalize()
     return ''.join(words)
+
+# or 
+""" def to_camel_case(text):
+    return text[:1] + text.title()[1:].replace('_', '').replace('-', '') """
