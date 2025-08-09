70391647dfea5ed033c49e483e7897ffa95cd3cd
Kenan Ben <gazokenan@gmail.com>
2025-08-09 22:57:26 +0800

    alt solution

--- Python/build_pile_cubes(6kyu).py
+++ Python/build_pile_cubes(6kyu).py
@@ -15,3 +15,11 @@ def find_nb(m):
         n += 1
         total += n ** 3
     return n if total == m else -1
+
+"""def find_nb(m):
+    i,sum = 1,1
+    while sum < m:
+        i+=1
+        sum+=i**3
+    return i if m==sum else -1
+    """
