e4ca02ad5efd504c209e9e32b71c8361da52a62c
Kenan Ben <gazokenan@gmail.com>
2025-08-11 22:25:14 +0800

    alt solution

--- Python/get_the_loop(5kyu).py
+++ Python/get_the_loop(5kyu).py
@@ -57,3 +57,13 @@ def loop_size(node):
 
     # voila
     return count """
+
+""" def loop_size(node):
+    index = {}
+    i = 0
+    while True:
+        if node in index:
+            return i - index[node]
+        index[node] = i
+        node = node.next
+        i += 1 """
