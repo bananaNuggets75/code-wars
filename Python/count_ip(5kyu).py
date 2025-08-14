b1935185a005e08e69090d5e28a6d8083723a307
Kenan Ben <gazokenan@gmail.com>
2025-08-14 21:22:06 +0800

    alt solution

--- Python/count_ip(5kyu).py
+++ Python/count_ip(5kyu).py
@@ -13,3 +13,13 @@ def ips_between(start, end):
         return (a << 24) + (b << 16) + (c << 8) + d  # bit shifting instead of powers
     
     return ip_to_int(end) - ip_to_int(start)
+
+""" from ipaddress import ip_address
+
+def ips_between(start, end):
+    return int(ip_address(end)) - int(ip_address(start))
+
+def ips_between(start, end):
+    a = sum([int(e)*256**(3-i) for i, e in enumerate(start.split('.'))])
+    b = sum([int(e)*256**(3-i) for i, e in enumerate(end.split('.'))])
+    return abs(a-b) """
