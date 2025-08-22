9af7ca2176fdfa1d9afd78fa1056fca451461341
Kenan Ben <gazokenan@gmail.com>
2025-08-22 22:03:22 +0800

    alt solutions

--- Python/roman_numerals_helper(4kyu).py
+++ Python/roman_numerals_helper(4kyu).py
@@ -81,3 +81,70 @@ class RomanNumerals:
                 result += roman_dict[roman_num[i]]
                 i += 1
         return result
+
+
+""" class RomanNumerals:
+
+    def to_roman(val):
+        onesRoman = ["","I","II","III","IV","V","VI","VII","VIII","IX"]
+        tensRoman = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"]
+        hundsRoman = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"]
+        thousRoman = ["","M","MM","MMM","MMMM"]
+            
+        ones =  onesRoman[val % 10]
+        tens = tensRoman[val // 10 % 10]
+        hunds = hundsRoman[val // 100 % 10]
+        thous = thousRoman[val // 1000]
+        
+            
+        return thous + hunds + tens + ones
+
+    def from_roman(roman_num):
+        out = 0 
+        mx = 0
+        for cur in map(lambda c: { 'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1 } [c], roman_num[::-1]):
+            if cur >= mx: 
+                out += cur
+                mx = cur
+            else:
+                out -= cur
+        
+        return out
+        
+
+from itertools import groupby
+
+"""A RomanNumerals helper object"""
+class RomanNumerals(object):
+    letters = [('M',1000), ('CM',900), ('D',500), ('CD',400), ('C',100), ('XC',90),
+               ('L',50), ('XL',40), ('X',10), ('IX',9), ('V',5), ('IV',4), ('I',1)]
+    
+    @classmethod
+    def to_roman(cls, val):
+        rom = []
+        for l, v in cls.letters:
+            m = val // v
+            rom += m*[l]
+            val -= m*v
+        return ''.join(rom)
+        
+    @classmethod
+    def from_roman(cls, rom):
+        cumu = 0
+        for l, v in cls.letters:
+            while rom[:len(l)] == l:
+                rom = rom[len(l):]
+                cumu += v
+            if not rom: break
+        return cumu
+        
+class RomanNumerals:
+    @staticmethod
+    def from_roman(s):
+        X=[dict(zip('MDCLXVI',(1e3,500,100,50,10,5,1)))[x]for x in s]
+        return int(sum((x,-x)[x<y]for x,y in zip(X,X[1:]))+X[-1])
+    @staticmethod
+    def to_roman(i,o=' I II III IV V VI VII VIII IX'.split(' ')):
+        r=lambda n:o[n]if n<10 else''.join(dict(zip('IVXLC','XLCDM'))[c]for c in r(n//10))+o[n%10]
+        return r(i)
+         """
